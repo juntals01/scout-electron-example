b00eb7718e5d8c0b3f813e9cc9350a546098aa9c
jun <norbertoqjr@gmail.com>
2025-07-24 11:02:01 +0800

    fix: runs tailwind on output

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -7,6 +7,7 @@ function App() {
 
   return (
     <>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
       <div>
         <a href='https://react.dev' target='_blank'>
           <img src={reactLogo} className='logo react' alt='React logo' />
